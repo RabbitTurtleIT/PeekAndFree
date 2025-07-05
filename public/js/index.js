de481ea97980c9752c4f6eb01026755278db696c
kimyw1018 <poppppp00@gmail.com>
2025-07-05 11:34:19 +0900

    where선택시 기능 추가

--- public/js/index.js
+++ public/js/index.js
@@ -4,6 +4,9 @@ document.addEventListener('DOMContentLoaded', () => {
     const btnMypage = document.getElementById('btn-mypage');
     const carousel = document.getElementById('mainCarousel');
     const indicators = document.querySelectorAll('.indicator-dot');
+    const whenContent = document.getElementById('when-content');
+    const whereContent = document.getElementById('where-content');
+
 
     const selectedImg = 'img/btn_selected.png';
     const unselectedImg = 'img/btn_unselected.png';
@@ -14,23 +17,41 @@ document.addEventListener('DOMContentLoaded', () => {
       });
     }
 
+    function activateButton(activeBtn, inactiveBtn) {
+    activeBtn.classList.add('active');
+    inactiveBtn.classList.remove('active');
+    }
+    
+    function showContent(type) {
+        if (type === 'when') {
+            whenContent.style.display = 'block';
+            whereContent.style.display = 'none';
+            whenContent.scrollIntoView({ behavior: 'smooth' });
+        } else {
+            whenContent.style.display = 'none';
+            whereContent.style.display = 'block';
+            whereContent.scrollIntoView({ behavior: 'smooth' });
+        }
+    }
+
     updateIndicators(0);
 
     // 슬라이드 변경될 때 인디케이터 갱신
     carousel.addEventListener('slide.bs.carousel', function (e) {
       updateIndicators(e.to);
     });     
-    function activateButton(activeBtn, inactiveBtn) {
-    activeBtn.classList.add('active');
-    inactiveBtn.classList.remove('active');
-     }
+
 
     btnWhen.addEventListener('click', () => {
         activateButton(btnWhen, btnWhere);
+        showContent('when');
     });
 
     btnWhere.addEventListener('click', () => {
         activateButton(btnWhere, btnWhen);
+        showContent('where');
+        selectResult.style.display = 'block';
+        window.scrollTo({ top: selectResult.offsetTop, behavior: 'smooth' });
     });
 
     btnMypage.addEventListener('click', () => {
