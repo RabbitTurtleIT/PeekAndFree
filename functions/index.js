42227d3cd291d4c9795bd0a536cf182d3d7d98b6
최종수 <chlwhdtn03@naver.com>
2025-07-15 22:59:14 +0900

    npm 관련 명령어 추가

--- functions/index.js
+++ functions/index.js
@@ -22,8 +22,6 @@ const { getFirestore } = require("firebase-admin/firestore");
 
 const app = initializeApp();
 
-require("dotenv").config();
-
 // Create and deploy your first functions
 // https://firebase.google.com/docs/functions/get-started
 
